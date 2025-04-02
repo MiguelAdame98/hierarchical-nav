7670f08b96b9865bd9a2ef2dd2668b0279fefbdd
MiguelAdame98 <79969150+MiguelAdame98@users.noreply.github.com>
2025-04-02 14:52:20 -0600

    try3

--- navigation_model/Processes/explorative_behaviour.py
+++ navigation_model/Processes/explorative_behaviour.py
@@ -39,7 +39,7 @@ class ExplorativeBehaviour():
         """
         print("why no explore?",abs(info_gain_coeff),(threshold / 500) ,abs(info_gain_coeff) < (threshold / 500),policy_G < (threshold/ 5),policy_G ,(threshold/ 5)  )
         #if (abs(info_gain_coeff) < (threshold / 1000) and policy_G < (threshold/ 10)) :
-        if (abs(info_gain_coeff) < (threshold / 500) and policy_G < (threshold/ 5)) :
+        if (abs(info_gain_coeff) < (threshold / 400) and policy_G < (threshold/ 5)) :
             self.agent_exploring = False
         else:
             self.agent_exploring = True
